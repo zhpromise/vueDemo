8a6f0e94eda43d31b012bc7a1b220959a85bc0e8
zhangfan <stanfanass@163.com>
2021-02-24 14:05:59 +0800

    Initial commit

---
+++ es6/js/main.js
@@ -0,0 +1,7 @@
+import abc from "./hello.js"
+import {name,add} from "./user.js"
+
+console.log(abc)
+console.log(name)
+console.log(add(1, 2))
+
---
+++ es6/js/user.js
@@ -0,0 +1,7 @@
+const name = "jack"
+const age = 21
+const add = (a, b) => {
+    return a + b;
+}
+
+export {name, age, add}
